1dc842388b6981dd3de5c885ae19265db734e7cb
crr0306 <2438167295@qq.com>
2020-08-31 16:03:02 +0800

    防止XSS SQL注入

---
+++ src/main/java/com/spring/base/filter/XssAndSqlFilter.java
@@ -0,0 +1,60 @@
+package com.spring.base.filter;
+
+
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+
+import javax.servlet.FilterChain;
+
+import javax.servlet.FilterConfig;
+
+import javax.servlet.ServletException;
+
+import javax.servlet.ServletRequest;
+
+import javax.servlet.ServletResponse;
+
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+
+
+
+
+@WebFilter(filterName = "XssAndSqlFilter",asyncSupported = true,urlPatterns ={} )
+public class XssAndSqlFilter implements Filter {
+
+    @Override
+    public void destroy() {
+
+// TODO Auto-generated method stub
+
+    }
+
+
+
+    @Override
+
+    public void doFilter(ServletRequest request, ServletResponse response,
+                         FilterChain chain) throws IOException, ServletException {
+
+        XssAndSqlHttpServletRequestWrapper xssRequest = new XssAndSqlHttpServletRequestWrapper(
+
+                (HttpServletRequest) request);
+
+        chain.doFilter(xssRequest, response);
+
+    }
+
+
+
+    @Override
+
+    public void init(FilterConfig arg0) throws ServletException {
+
+// TODO Auto-generated method stub
+
+    }
+
+}
